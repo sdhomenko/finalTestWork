33d10bfc988735c349e0754abd61c9f7ca4fd0d6
sergeyhomenko <sdhomenko@gmail.com>
2022-06-11 19:06:30 +0300

    Test Work Over

--- TestWork1/Program.cs
+++ TestWork1/Program.cs
@@ -1,19 +1,33 @@
-﻿//Написать программу, которая из имеющегося массива строк формитрует массив из строк, 
-//длина которых меньше либо равна 3 символа.
+﻿//Написать программу, которая из имеющегося массива строк 
+//формитрует массив из строк, длина которых меньше либо равна 3 символа.
 
 //Создаем массив из строк
 string[] CreateStringsMassive()
 {
-
     Console.Write("Задайте размер массива: ");
     int s = Convert.ToInt32(Console.ReadLine());
     string[] result = new string[s];
     for (int i = 0; i < result.Length; i++)
     {
-        Console.Write($"Введите {i+1} элемент массива: ");
+        Console.Write($"Введите {i + 1} элемент массива: ");
         result[i] = Console.ReadLine();
     }
     return result;
+}
+
+string[] OptimizeMassive(string[] text)
+{
+    string[] newtext = new string[text.Length];
+    int count = 0;
+    for (int i = 0; i < text.Length; i++)
+    {
+        string element = String.Empty;
+        element = text[i];
+        if (element.Length <= 3) newtext[i - count] = text[i];
+        else count++;
+    }
+    Array.Resize(ref newtext, text.Length - count);
+    return newtext;
 }
 
 //Печать массива
@@ -21,10 +35,14 @@ void PrintMassive(string[] text)
 {
     for (int i = 0; i < text.Length; i++)
     {
-        Console.Write($"{text[i]} ");
+        Console.Write($"'{text[i]}' ");
     }
+    Console.WriteLine();
 }
 
 string[] text = CreateStringsMassive();
-Console.WriteLine();
-PrintMassive (text);
+Console.WriteLine("Заданный массив");
+PrintMassive(text);
+string[] newtext = OptimizeMassive(text);
+Console.WriteLine("Итоговый массив");
+PrintMassive(newtext);
